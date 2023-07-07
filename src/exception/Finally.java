0bb313c1e3402b9e0f6c7560de2f6b102f40b058
Natália Diotto <diottonatalia@gmail.com>
2023-07-07 11:25:30 -0400

    Implementing second Finally block

--- src/exception/Finally.java
+++ src/exception/Finally.java
@@ -11,8 +11,14 @@ public class Finally {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally { //força a execução, sempre é chamado
-            System.out.println("Finally...");
-            sc.close();
+            System.out.println("Finally #01...");
+            //sc.close();
+        }
+
+        try {
+            System.out.println(7 / sc.nextInt());
+        } finally { //em todas as situações o finally é chamado, mesmo sem o block catch
+            System.out.println("Finally #02...");
         }
 
         System.out.println("The end.");
