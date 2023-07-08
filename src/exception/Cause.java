8ef342d0ca1339e012ec0cd70d4c250ac3b8730e
Natália Diotto <diottonatalia@gmail.com>
2023-07-08 19:55:40 -0400

    Treating methodA exception

--- src/exception/Cause.java
+++ src/exception/Cause.java
@@ -3,12 +3,22 @@ package exception;
 public class Cause {
 
     public static void main(String[] args) {
-        methodA(null);
-
+        try {
+            methodA(null);
+        } catch (Exception e) {
+            if (e.getCause() != null) {
+                System.out.println(e.getCause().getMessage());
+            }
+        }
     }
 
     static void methodA(Student student) {
-        methodB(student);
+        try {
+            methodB(student);
+        } catch (Exception cause) { //posso lançar outra exceção dentro do catch
+            throw new IllegalArgumentException(cause);
+        }
+
     }
 
     static void methodB(Student student) {
