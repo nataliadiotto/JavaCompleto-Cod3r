4cfd617531df9389e5fc713659721f1098485e51
Natália Diotto <diottonatalia@gmail.com>
2023-07-15 16:00:30 -0400

    Generating Error Messages

--- src/exception/NegativeNumberException.java
+++ src/exception/NegativeNumberException.java
@@ -1,4 +1,16 @@
 package exception;
 
-public class NegativeNumberException {
+public class NegativeNumberException extends RuntimeException {
+
+    private String attributeName;
+
+    public NegativeNumberException(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    public String getMessage() {
+        return String.format("The attribute %s is negative",
+                attributeName);
+    }
+
 }
