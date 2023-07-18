2a338006108aa141352332ed70ac8a3af19e0e34
Natália Diotto <diottonatalia@gmail.com>
2023-07-18 17:41:29 -0300

    Implementing Behavior Filter

--- src/streams/Filter.java
+++ src/streams/Filter.java
@@ -1,7 +1,5 @@
 package streams;
 
-import oo.composicao.Aluno;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -9,21 +7,23 @@ import java.util.function.Predicate;
 
 public class Filter {
     public static void main(String[] args) {
-        Student std1 = new Student("Anna", 7.8);
-        Student std2 = new Student("Bia", 5.8);
-        Student std3 = new Student("Daniel", 9.8);
-        Student std4 = new Student("Gui", 6.8);
-        Student std5 = new Student("Rebeca", 7.1);
-        Student std6 = new Student("Pedro", 8.8);
+        Student std1 = new Student("Anna", 7.8, false);
+        Student std2 = new Student("Bia", 5.8, true);
+        Student std3 = new Student("Daniel", 9.8, true);
+        Student std4 = new Student("Gui", 6.8, true);
+        Student std5 = new Student("Rebeca", 7.1, true);
+        Student std6 = new Student("Pedro", 8.8, false);
 
         List<Student> students = Arrays.asList(std1, std2, std3, std4, std5, std6);
 
-        Predicate<Student> approved = student -> student.grade >= 7;
+        Predicate<Student> approved = student -> student.grade >= 7; //&& student.goodBehavior;
+        Predicate<Student> goodBehavior = student -> student.goodBehavior;
         Function<Student, String> message =
                 student -> "Congratulations, " + student.name + "! You have been approved.";
 
         students.stream()
                 .filter(approved)
+                .filter(goodBehavior)
                 .map(message)
                 .forEach(System.out::println);
 
--- src/streams/Student.java
+++ src/streams/Student.java
@@ -4,9 +4,11 @@ public class Student {
 
     final String name;
     final double grade;
+    final boolean goodBehavior;
 
-    public Student(String name, double grade) {
+    public Student(String name, double grade, boolean goodBehavior) {
         this.name = name;
         this.grade = grade;
+        this.goodBehavior = goodBehavior;
     }
 }
