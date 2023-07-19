8a9fe29344a57cb62f2b1dbcfaac41888c73a62e
Natália Diotto <diottonatalia@gmail.com>
2023-07-19 10:20:21 -0300

    Implementing Movement Erros Validation

--- src/streams/VolleyballMVP.java
+++ src/streams/VolleyballMVP.java
@@ -8,14 +8,15 @@ import java.util.function.Predicate;
 public class VolleyballMVP {
     public static void main(String[] args) {
 
-        Player p1 = new Player("Gabriela Guimaraes", "Brazil", "Outside Hitter", 87, 28, 43, 10);
+        Player p1 = new Player("Paola Egonu", "Italy", "Opposite Hitter", 87, 28, 43, 10);
         Player p2 = new Player("Thaisa Daher", "Brazil", "Middle blocker", 86, 11, 43, 10);
         Player p3 = new Player("Melissa Vargas", "Turkey", "Opposite hitter", 85, 37, 40, 13);
         Player p4 = new Player("Micha Hancock", "The USA", "Setter", 60, 65, 39, 10);
 
         List<Player> players = Arrays.asList(p1, p2, p3, p4);
 
-        Predicate<Player> isEfficient = player -> player.positiveMovements > player.errors && player.errors <= 20;
+        Predicate<Player> isEfficient = player -> player.positiveMovements > player.errors;
+        Predicate<Player> avrgErrors = player -> player.errors <= 20;
         Predicate<Player> setAverage = player -> player.setsPlayed - player.setsLost >= 30;
 
         Function<Player, String> message =
@@ -26,6 +27,7 @@ public class VolleyballMVP {
 
         players.stream()
                 .filter(isEfficient)
+                .filter(avrgErrors)
                 .filter(setAverage)
                 .map(message)
                 .forEach(System.out::println);
