730d428508c8a3a2e9a8b657c8f2ad662dcef575
Natália Diotto <diottonatalia@gmail.com>
2023-07-21 20:13:01 -0300

    Treating Exceptions

---
+++ src/exception/Basic.java
@@ -0,0 +1,28 @@
+package exception;
+
+public class Basic {
+    public static void main(String[] args) {
+
+        Student s1 = null;
+
+        try {
+            printStudent(s1);
+        } catch (Exception exception) {
+            System.out.println("Error printing user's name");
+        }
+
+        try {
+            System.out.println(7/0);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("The end");
+
+        //
+    }
+
+    public static void printStudent(Student student){
+        System.out.println(student.name);
+    }
+}
